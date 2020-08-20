533b6ebff7e17078edd43f555c7a21c0b31900e8
Alexandr Yatsenko <iosiguides@gmail.com>
2020-08-20 17:00:18 +0300

    Update file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 new WOW().init();
+
+// Slider
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active',
+        clickable: true
+    },
+});
